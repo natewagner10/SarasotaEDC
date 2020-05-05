3df1a0baf042768f33f7591e562c6f7e7a2ced2b
natewagner10 <54585457+natewagner10@users.noreply.github.com>
2020-05-05 16:47:44 -0400

    Update maskRcnn_objectDetection.py

--- mask_rcnn_files/maskRcnn_objectDetection.py
+++ mask_rcnn_files/maskRcnn_objectDetection.py
@@ -35,102 +35,79 @@ from xml.etree import ElementTree
 
 
 class myMaskRCNNConfig(Config):
-    # give the configuration a recognizable name
     NAME = "MaskRCNN_config"
- 
-    # set the number of GPUs to use along with the number of images
-    # per GPU
+
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
  
-    # number of classes (we would normally add +1 for the background)
-     # kangaroo + BG
     NUM_CLASSES = 2+1
    
-    # Number of training steps per epoch
     STEPS_PER_EPOCH = 240
     
-    # Learning rate
     LEARNING_RATE=0.006
-    
-    # Skip detections with < 90% confidence
+     
     DETECTION_MIN_CONFIDENCE = 0.40
     
     RPN_NMS_THRESHOLD = 0.20
     
-    # setting Max ground truth instances
     MAX_GT_INSTANCES=63
     
     DETECTION_MAX_INSTANCES = 63
     
     VALIDATION_STEPS = 60
     
-    
     RPN_ANCHOR_SCALES = (16, 32, 64, 128, 256)
     
 
-
 config = myMaskRCNNConfig()
-
-
-
 config.display()
 
 
-
-
-
 class surveyDataset(Dataset):
-    # load the dataset definitions
+
     def load_dataset(self, dataset_dir, is_train=True):
         
-        # Add classes. We have only one class to add.
+
         self.add_class("dataset", 1, "box")
         self.add_class("dataset", 2, "checked_box")
         
-        # define data locations for images and annotations
+
         images_dir = dataset_dir + '/images_sharp/'
         annotations_dir = dataset_dir + '/annotations/'
         
-        # Iterate through all files in the folder to 
-        #add class, images and annotaions
         cnt = 0
         for filename in listdir(images_dir):
-            #print(filename)
+        
             cnt += 1
-            # extract image id   
+      
             image_id = filename[:-4]
-            
-            # skip bad images
+                  
             if image_id in have:
                 continue
             if image_id in ['.DS_Store']:
                 continue
             if image_id in ['.DS_S.xml']:
                 continue
-            # skip all images after 150 if we are building the train set
+            
             if is_train and cnt >= 241:
                 continue
-            # skip all images before 150 if we are building the test/val set
+           
             if not is_train and cnt <= 241:
                 continue
-            
-            # setting image file
+     
             img_path = images_dir + filename
             
-            # setting annotations file
             ann_path = annotations_dir + image_id + '.xml'
             
-            # adding images and annotations to dataset
             self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
-# extract bounding boxes from an annotation file
+
     def extract_boxes(self, filename):
         
-        # load and parse the file
+
         tree = ElementTree.parse(filename)
-        # get the root of the document
+
         root = tree.getroot()
-        # extract each bounding box
+
         obj_class = list()
         for cl in root.findall('.//name'):
             obj_class.append(cl.text)
@@ -144,11 +121,11 @@ class surveyDataset(Dataset):
             coors = [xmin, ymin, xmax, ymax]
             boxes.append(coors)
         
-        # extract image dimensions
+       
         width = int(root.find('.//size/width').text)
         height = int(root.find('.//size/height').text)
         return boxes, width, height, obj_class
-# load the masks for an image
+
     """Generate instance masks for an image.
        Returns:
         masks: A bool array of shape [height, width, instance count] with
@@ -156,10 +133,9 @@ class surveyDataset(Dataset):
         class_ids: a 1D array of class IDs of the instance masks.
      """
     def load_mask(self, image_id):
-        # get details of image
+
         info = self.image_info[image_id]
         
-        # define anntation  file location
         path = info['annotation']
         
         # load XML
